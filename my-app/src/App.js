621d4af3969d76da55fee4de8ef2e8227577a6a4
Reshma Abraham <reshmaabraham@Reshmas-MacBook-Pro.local>
2020-01-08 16:17:15 +1100

    adding crap

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+ import React from "react"
+// import logo from ".logo.svg";
+// import "./App.css";
+
+const App  =() =>  {
+
+    const APP_ID = '321e254c';
+
+    const APP_KEY = "f40f280708db05a4c629f573d25ec100";
+
+    const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    return (
+        <div className="App">
+            <form className = "search-form">
+                <input className = "search-bar" type="text"/>
+                <button className ="search-button" type ="submit"> 
+                    Search 
+                </button>
+            </form>
+        </div> 
+    );
+};
+
+export default App;
