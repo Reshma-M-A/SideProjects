c955684a0c1c2b5eaf7979335302889dfbf275cf
Reshma Abraham <reshmaabraham@Reshmas-MacBook-Pro.local>
2020-01-08 17:45:02 +1100

    start of my food app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
-import logo from ".logo.svg";
+// eslint-disable-next-line
+import logo from "./logo.svg"; 
 import "./App.css";
 
 const App  =() =>  {
@@ -7,7 +8,7 @@ const App  =() =>  {
     const APP_ID = '321e254c';
 
     const APP_KEY = "f40f280708db05a4c629f573d25ec100";
-
+    // eslint-disable-next-line
     const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
     return (
         <div className="App">
