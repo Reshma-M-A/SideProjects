c3b9f100af215ca57894d6100c0399aea4536982
Reshma Abraham <reshmaabraham@Reshmas-MacBook-Pro.local>
2020-01-09 18:55:47 +1100

    useEffect is only being run once since we added the second argument []

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 // eslint-disable-next-line
 import logo from "./logo.svg"; 
 import "./App.css";
@@ -10,14 +10,23 @@ const App  =() =>  {
     const APP_KEY = "f40f280708db05a4c629f573d25ec100";
     // eslint-disable-next-line
     const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+
+    const [counter, setCounter] = useState(0);
+
+
+    useEffect(() => {
+      console.log("Effect has been run");
+    }, []);
+
+
     return (
         <div className="App">
             <form className = "search-form">
                 <input className = "search-bar" type="text"/>
-                <button className ="search-button" type ="submit"> 
-                    Search 
-                </button>
+                <button className ="search-button" type ="submit"> Search </button>
             </form>
+            <h1 onClick ={() => setCounter(counter+1)} >{counter}</h1> 
         </div> 
     );
 };
