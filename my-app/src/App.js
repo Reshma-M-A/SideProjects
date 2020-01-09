3df905b121088d1701a48dba5a3611b9e9aeb204
Reshma Abraham <reshmaabraham@Reshmas-MacBook-Pro.local>
2020-01-09 19:11:51 +1100

    putting the recipes json data into useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState} from "react";
 // eslint-disable-next-line
 import logo from "./logo.svg"; 
 import "./App.css";
@@ -8,17 +8,21 @@ const App  =() =>  {
     const APP_ID = '321e254c';
 
     const APP_KEY = "f40f280708db05a4c629f573d25ec100";
-    // eslint-disable-next-line
-    const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  
 
-    const [counter, setCounter] = useState(0);
-
+    const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
-      console.log("Effect has been run");
-    }, [counter]);
+      getReceipes();
+    }, []);
 
+    // fetching all of our data
+    // making async calls
+    const getReceipes = async () => {
+      //not going to arrive instantly
+      const response = await fetch (`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+    }
 
     return (
         <div className="App">
@@ -26,7 +30,6 @@ const App  =() =>  {
                 <input className = "search-bar" type="text"/>
                 <button className ="search-button" type ="submit"> Search </button>
             </form>
-            <h1 onClick ={() => setCounter(counter+1)} >{counter}</h1> 
         </div> 
     );
 };
